b9201e93111f9197472526c903ff29b6a2a44498
Zain2012-byte <muzaffartawseefzain@gmail.com>
2024-10-05 20:02:19 +0530

    Zain project 2

---
+++ Storing-Birthdaydates.py
@@ -0,0 +1,17 @@
+#Storing birthday dates of my friends
+  
+umer=12/10/2012
+rahat=9/8/2012
+tejas=8/5/2013
+anas=12/24/2012
+huzaif=13/15/2011
+#Printing birthday's best of luck
+print('Happy Birthday umer stay may god bless you \n ')
+
+print('Happy Birthday rahat stay may god bless you \n ')
+
+print('Happy Birthday anas  may god bless you \n ')
+
+print('Happy Birthday tejas  may god bless you \n ')
+
+print('Happy Birthday huzaif  may god bless you \n ')
